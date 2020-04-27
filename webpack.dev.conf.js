47a96962d86809c838eb89db211c524c8fee516d
Konstantin Kulabukhov <konstantin.kulabukhov@gmail.com>
2020-04-27 20:45:16 +0700

    fix webpack

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -1,22 +1,23 @@
-const merge = require("webpack-merge");
-const baseWebpackConfig = require("./webpack.base.conf");
+const merge = require('webpack-merge');
+const baseWebpackConfig = require('./webpack.base.conf');
 
 const devWebpackConfig = merge(baseWebpackConfig, {
-  mode: "development",
-  devtool: "cheap-module-eval-source-map",
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: baseWebpackConfig.externals.paths.dist,
     historyApiFallback: true,
     overlay: true,
     port: 3000,
+
     proxy: {
-      "/api/**": {
-        target: "http://localhost:8000",
+      '/api/**': {
+        target: 'http://localhost:8000',
         secure: false,
-        changeOrigin: true
-      }
-    }
-  }
+        changeOrigin: true,
+      },
+    },
+  },
 });
 
 module.exports = new Promise(resolve => {
