dbfa9523919133a34a3626e725e514fd3b408042
Konstantin Kulabukhov <konstantin.kulabukhov@gmail.com>
2020-07-02 16:09:24 +0700

    fix load file for ios

--- src/components/ProfilePage/ConfigurationFilesTabs/Link/Link.tsx
+++ src/components/ProfilePage/ConfigurationFilesTabs/Link/Link.tsx
@@ -12,15 +12,6 @@ interface IComponentProps {
 type TProps = IComponentProps;
 
 const Link = ({ link, linkName }: TProps) => {
-  const getFileType = () => {
-    switch (link.ext) {
-      case 'png':
-        return 'image/png';
-      default:
-        return 'text/plain';
-    }
-  };
-
   const getResponseType = () => {
     switch (link.ext) {
       case 'png':
@@ -37,12 +28,11 @@ const Link = ({ link, linkName }: TProps) => {
     }
   };
 
-  const fileType = getFileType();
   const responseType = getResponseType();
 
   const isIos = window.navigator.userAgent.search(/(iPhone|iPad)/) > -1;
 
-  const openFileOnFileReader = (blob: Blob): void => {
+  const downloadFileWithFileReader = (blob: Blob): void => {
     const reader = new FileReader();
     reader.readAsDataURL(blob);
 
@@ -51,8 +41,16 @@ const Link = ({ link, linkName }: TProps) => {
       if (typeof result === 'string') {
         const virtualLink = document.createElement('a');
 
+        virtualLink.target = '_self';
+        virtualLink.style.display = 'none';
         virtualLink.href = result;
+        virtualLink.download = link.fileName;
+        virtualLink.innerHTML = link.fileName;
+        document.body.appendChild(virtualLink);
         virtualLink.click();
+        if (virtualLink.parentNode) {
+          virtualLink.parentNode.removeChild(virtualLink);
+        }
         URL.revokeObjectURL(result);
       }
     };
@@ -82,10 +80,10 @@ const Link = ({ link, linkName }: TProps) => {
       successCallback: (res: AxiosResponse<any>) => {
         const { data } = res;
 
-        const blob = responseType === 'blob' ? data : new Blob([data], { type: fileType });
+        const blob = responseType === 'blob' ? data : new Blob([data]);
 
         if (isIos) {
-          openFileOnFileReader(blob);
+          downloadFileWithFileReader(blob);
         } else {
           downloadFile(blob);
         }
